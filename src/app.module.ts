c87f8bf5136290ad12aa32e50802b3abfb146282
Solist-Alexander <fcco1233@gmail.com>
2024-06-27 21:23:59 +0300

    data

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+
+import { GlobalExceptionFilter } from './common/http/global-exception.filter';
+import configuration from './configs/configs';
+import { AuthModule } from './modules/auth/auth.module';
+import { LoggerModule } from './modules/logger/logger.module';
+import { UserModule } from './modules/user/user.module';
+import { PostgresModule } from './postgres/postgres.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    PostgresModule,
+    LoggerModule,
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
